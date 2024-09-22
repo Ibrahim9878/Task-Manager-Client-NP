1acdaa6ba2940ef9b6184733d77adc0fc996863f
Ibrahim9878 <talibliibrahim28@gmail.com>
2024-09-22 11:53:03 +0400

    Add project files.

---
+++ Task Manager Client NP/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Windows;
+
+namespace Task_Manager_Client_NP;
+
+
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    HttpClient Client = new();
+    HttpRequestMessage message = new();
+    private List<string> processes;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public List<string> Processes { get => processes; set { processes = value; NotifyPropertyChanged(); } }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+        Processes = new();
+    }
+
+    private async void GETButton_Click(object sender, RoutedEventArgs e)
+    {
+        message = new();
+        Client = new();
+        message.RequestUri = new Uri(@"http://localhost:27001/");
+        message.Method = HttpMethod.Get;
+        message.Headers.Add("GET", Processes);
+
+        var response = await Client.GetAsync(message.RequestUri);
+        var json = await response.Content.ReadAsStringAsync();
+        Processes = JsonSerializer.Deserialize<List<string>>(json);
+
+    }
+
+    private async void RUNButton_Click(object sender, RoutedEventArgs e)
+    {
+        var SelectedItemName = RunBox.Text;
+        message = new();
+        Client = new();
+        Client.DefaultRequestHeaders.Add("POST", "application.json");
+        message.RequestUri = new Uri(@"http://localhost:27001/");
+        message.Method = HttpMethod.Post;
+        message.Headers.Add("POST", SelectedItemName);
+        
+              
+       
+        var response = await Client.PostAsync(message.RequestUri,message.Content);
+        var json = await response.Content.ReadAsStringAsync();
+        MessageBox.Show(json);
+    }
+
+    private async void KILLButton_Click(object sender, RoutedEventArgs e)
+    {
+        message = new();
+        Client = new();
+        var SelectedItemName = (ProcessBox.SelectedItem as Process).ProcessName;
+        message = new();
+        message.RequestUri = new Uri(@"http://localhost:27001/");
+        message.Headers.Add("DELETE", SelectedItemName);
+        message.Method = HttpMethod.Delete;
+        var response = await Client.DeleteAsync(message.RequestUri);
+        var json = await response.Content.ReadAsStringAsync();
+        MessageBox.Show(json);
+    }
+}
---
+++ Task Manager HttpListener NP/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Text.Json;
+
+var listener = new HttpListener();
+listener.Prefixes.Add(@"http://localhost:27001/");
+listener.Start();
+
+
+while (true)
+{
+    var context = listener.GetContext();
+    var request = context.Request;
+    var response = context.Response;
+    StreamWriter writer = new StreamWriter(response.OutputStream);
+    if (request.HttpMethod == "GET")
+    {
+        var processes = Process.GetProcesses().ToList().Select(p => p.ProcessName).ToList();
+        string json = JsonSerializer.Serialize(processes);
+        writer.WriteLine(json);
+    }
+    else if (request.HttpMethod == "DELETE")
+    {
+        var killProcessName = response.Headers["DELETE"];
+
+        var pr = Process.GetProcesses().FirstOrDefault(p => p.ProcessName == killProcessName).Id;
+        Process.GetProcessById(pr).Kill();
+
+        writer.WriteLine("ProcessKilled");
+    }
+    else if (request.HttpMethod == "POST")
+    {
+        var runProcessName = response.Headers["POST"];
+
+        Process.Start(runProcessName);
+
+        writer.WriteLine("Process Started");
+    }
+    writer.Close();
+}
+
+
+
